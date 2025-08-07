1f1c0022a9be3e64b56a990305b56bd3dd6089a1
troy0915 <johntroydolom7@gmail.com>
2025-08-07 15:32:50 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_19_Online_Attendance_Tracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> attendees = new List<string>();
+            string input;
+
+            Console.WriteLine("Enter student names (one per line, blank to finish):");
+            {
+                Console.Write("> ");
+                input = Console.ReadLine().Trim();
+
+                while (true)
+                {
+                    if (string.IsNullOrWhiteSpace(input))
+                    {
+                        break; 
+                    }
+
+                    if (!ContainsName(attendees, input))
+                    {
+                        attendees.Add(input);
+                        Console.WriteLine($"Added: {input}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{input} already logged - not counted again");
+                    }
+                }                                             
+            }
+            attendees.Sort(StringComparer.OrdinalIgnoreCase);
+
+            Console.WriteLine("\nAttendance Report");
+            Console.WriteLine("-----------------");
+            Console.WriteLine($"Total unique attendees: {attendees.Count}");
+            Console.WriteLine("\nAttendees (alphabetical order):");
+
+            foreach (string student in attendees)
+            {
+                Console.WriteLine(student);
+            }
+        }
+        static bool ContainsName(List<string> list, string name)
+        {
+            foreach (string item in list)
+            {
+                if (item.Equals(name, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+
+
+
+
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Activity 19 Online Attendance Tracker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Activity 19 Online Attendance Tracker")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0605b609-7a06-498c-ba9a-1c9c3dacbae5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
